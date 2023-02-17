b11a202662983045390a3ed77735b7e39ed7ce1b
Arshdeep Singh Chhabra <arshdeepchhabra1712@gmail.com>
2023-02-17 12:00:43 +0530

    final commit

---
+++ Js_Operators.js
@@ -0,0 +1,69 @@
+//                  JS Arithmetic operators:
+
+// We can perform Arithmetic operations on literals, variables and expressions
+
+
+// The (+) Operator:
+
+// let a=8;
+// let b=8;
+// console.log(a+b)
+
+// The (-) Operator:
+
+// let a = 3-9;
+// console.log(a)
+
+// The (*) Operator:
+
+// let a=5;
+// let b=(4*3)*a
+// console.log(b)
+
+// The (/) Operator:
+// Division operator always returns with quotent (upper part of division)
+
+// let a=2;
+// let b=12;
+// console.log(b/a)
+
+// The (%) Operator:
+// Modulus operator always returns with remainder (lower part of division)
+
+// let a=9;
+// let b=3;
+// console.log(a%b)
+
+// The (++) Operator:
+// Increases value by 1
+
+// let a=4;
+// a++
+// console.log(a)
+
+// The (--) Operator:
+// Decreases value by 1
+
+// let a=5;
+// a--
+// console.log(a)
+
+// The (**) Operator:
+// it gives you the power of the  operand 
+
+// let a=3;
+// console.log(a**3)
+
+
+//      Operator precedence
+
+// It describes, in which order will arithmetic operation performs:
+//  DMAS
+
+// let a=9+3*7;
+// console.log(a);
+
+// We can also change the order by adding brackets:
+
+// let a=(9+3)*7
+// console.log(a);
---
+++ js_Numbers.js
@@ -0,0 +1,109 @@
+// Scientific (exponant) way of writing numbers:
+
+// let a=123e2;
+// let b= 123e-2;
+// console.log(a)
+// console.log(b)
+
+// Adding Numbers with Numbers
+
+// let a=20;
+// let b=80;
+// console.log(a+b)
+
+// Adding two strings
+
+// let a="10"
+// let b="20"
+// console.log(a+b)
+
+// Adding Numbers with strings
+
+// let a=20;
+// let b="30"
+// console.log(a+b)
+
+// Math operation will work on strings also except '+'
+
+// Multiplying strings
+
+// let a='10';
+// let b='8';
+// console.log(a*b)
+
+// Adding strings won't work infact it will concatenate the string, '+' operator is used to concatenate string
+
+// let a='20';
+// let b='80';
+// console.log(a+b)
+
+//  NAN (Not a number)
+// Below mentioned code will give us o/p with NAN
+
+// let a=90;
+// let b='Apple';
+// console.log(a*b)
+
+// But if string is in numeric form then it will return a number
+
+// let a=20;
+// let b='100'
+// console.log(a*b)
+
+// Here's a Js method "typeof" which returns the type of datatype
+// let a=true;
+// console.log(typeof a)
+
+// Js Number methods:
+
+// 1> tostring() (Returns a number as a string, and it changes the datatype)
+
+// let a=25;
+// console.log(typeof(a.toString()))
+// let b='25'
+// console.log(typeof(b.toString()))
+
+
+
+// 2> valueof() (Returns number as a number, and it changes the datatype)
+
+// let a='9';
+// console.log(typeof(a.valueOf()))
+// let b=9
+// console.log(typeof(b.valueOf()))
+// let c=true
+// console.log(typeof(c.valueOf()))
+
+// 3> Number() (Converts js variable to number)
+
+// let a=true;
+// console.log(Number(a))
+// let b='10,9'
+// console.log(Number(b))
+
+// 4> parseint() (parses string and returns whole number, it only returns the first value)
+
+// let a='4'
+// console.log(typeof(parseInt(a)))
+// let b='abc 10'
+// console.log(parseInt(b))
+// let c='3abc'
+// console.log(parseInt(c))
+
+// 5> parsefloat() (parses string and returns number, it only returns the first value)
+// let a='5'
+// console.log(typeof(parseFloat(a)))
+// let b=10565659656e-5
+// console.log(parseFloat(b))
+// let c='10 years'
+// console.log(parseFloat(c))
+// let d='years 10'
+// console.log(parseFloat(d))
+
+
+            
+
+
+
+
+
