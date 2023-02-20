0fee57a2279f3e091c702de3069e3c07c5b03139
Arshdeep Singh Chhabra <arshdeepchhabra1712@gmail.com>
2023-02-20 13:02:26 +0530

    final commit

--- Js_Operators.js
+++ Js_Operators.js
@@ -1,54 +1,69 @@
-//                  JS Arithmetic operators:
+//                  JS Operators
+
+ 
+//              Different type of operators:
+
+// Arithmetic Operators
+// Assignment Operators
+// Comparision Operators
+// Logical Operators
+// Conditional Operators
+// Type Operators
+
+
+
+
+//                   Arithmetic operators
 
 // We can perform Arithmetic operations on literals, variables and expressions
 
 
-// The (+) Operator:
+// The (+ : Plus) Operator:
 
 // let a=8;
 // let b=8;
 // console.log(a+b)
 
-// The (-) Operator:
+// The (- : Subtraction) Operator:
 
 // let a = 3-9;
 // console.log(a)
 
-// The (*) Operator:
+// The (* : Multiplication) Operator:
 
 // let a=5;
 // let b=(4*3)*a
 // console.log(b)
 
-// The (/) Operator:
+// The (/ : Division) Operator:
 // Division operator always returns with quotent (upper part of division)
 
 // let a=2;
 // let b=12;
 // console.log(b/a)
 
-// The (%) Operator:
+// The (% : Modulus) Operator:
 // Modulus operator always returns with remainder (lower part of division)
 
 // let a=9;
 // let b=3;
 // console.log(a%b)
 
-// The (++) Operator:
+// The (++ : Increment) Operator:
 // Increases value by 1
 
 // let a=4;
 // a++
 // console.log(a)
 
-// The (--) Operator:
+// The (-- : Decrement) Operator:
 // Decreases value by 1
 
 // let a=5;
 // a--
 // console.log(a)
 
-// The (**) Operator:
+// The (** : Exponentiation) Operator:
 // it gives you the power of the  operand 
 
 // let a=3;
@@ -67,3 +82,66 @@
 
 // let a=(9+3)*7
 // console.log(a);
+
+
+
+
+//      Assignment Operators
+
+// Assignment Operators means to assign values to Variables
+
+// The (= : Simple Assignment) Operator:
+
+// let a=5;
+// let b=10;
+// let c=a+b;
+// console.log(c)
+
+// The (+= : Addition Assignment) Operator:
+
+// let a=3;
+// a+=5;
+// console.log(a)
+
+// let a="Hello"
+// a+=" World"
+// console.log(a)
+
+// The (-= : Subtraction Assignment) Operator:
+
+// let a=4;
+// a-=9
+// console.log(a)
+
+// let a="Hello"
+// a-=" World"
+// console.log(a)
+
+// The (*= : Multiplication Assignment) Operator:
+
+// let a=5;
+// a*=3;
+// console.log(a)
+
+// let a="Hello"
+// a*=" World"
+// console.log(a)
+
+// The (/= : Division Assignment) Operator:
+
+// let a=9;
+// a/=3;
+// console.log(a)
+
+// The (%= : Remainder Assignment) Operator:
+
+// let a=7;
+// a%=2;
+// console.log(a)
+
+// The (**= : Exponentiation Assignment) Operator:
+
+// let a=2;
+// a**=3;
+// console.log(a)
+
