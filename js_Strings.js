6687310f0a4dd8ca90ed8b004fdd755202d9c19e
Arshdeep Singh Chhabra <arshdeepchhabra1712@gmail.com>
2023-02-23 16:36:35 +0530

    final commit

---
+++ js_Strings.js
@@ -0,0 +1,23 @@
+//          JS Strings
+
+// Js Strings are used to store texts
+
+// let a="Aayush"
+// console.log(a)
+
+// We can use single and double quotes to form a string
+
+// let a='Aayush'
+// let b="Ustat"
+// console.log(a+'\n'+b)
+
+// We can use quotes inside quoted but condition is they shouldn't match
+
+// let a="Aayush shouldn't go with 'Ustat'"
+// console.log(a)
+
+// We can get the string length by using string inbuilt property
+// It also counts the spaces and commas
+
+// let a="Aayush is a 'good programmer'"
+// console.log(a.length)
