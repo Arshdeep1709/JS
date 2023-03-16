9fc64b54096198e9636ec8daf67d995d9875e615
Arshdeep Singh Chhabra <arshdeepchhabra1712@gmail.com>
2023-03-16 16:22:26 +0530

    _

--- js_Strings.js
+++ js_Strings.js
@@ -21,3 +21,59 @@
 
 // let a="Aayush is a 'good programmer'"
 // console.log(a.length)
+
+
+//      Escape Character
+
+// for ex: we have to write : "Aayush is a "good Programmer""
+// because we are using double quotes in the string
+// for such problems we use escape characters:
+
+// (/) bakslash turns special character into string
+
+// let a="Aayush is a \"good Programmer\""
+// console.log(a)
+
+// let a='Aayush is \'great\''
+// console.log(a)
+
+// let a='It\'s okk'
+// console.log(a)
+
+// Six other escape sequence:
+
+// \n : New line
+// \t : Horizontal tab
+// \r : Carraige return
+// \f : Form feed
+// \b : Backspace
+// \v : vertical tab
+
+
+//          Js String methods
+
+// Javascript string length method
+
+// The length property returns length of string
+// It also counts the spaces and commas
+
+// let a = 'Aayush, Ustat'
+// let b = a.length;
+// console.log(b)
+
+
+
+// Javascript String slice() method:
+
+// It accepts two parameters (start,end)
+// according to the parameters it gives u sliced output
+// It also counts the spaces and commas
+// It counts position from 0
+// If position is negetive it counts from the end
+
+let a = "abcdefghijklmnopqrstuvwxyz";
+let b = a.slice(5,10)
+let c = a.slice(-4)
+let d = a.slice(-3,-9)
+console.log(d)
+
